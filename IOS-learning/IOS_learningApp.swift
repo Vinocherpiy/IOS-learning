479a3b3c9fd3e3724881689663822c9dd184d522
Vinocherpiy <tipumbady@gmail.com>
2025-08-26 21:28:27 +0300

    Initial Commit

---
+++ IOS-learning/IOS_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_learningApp.swift
+//  IOS-learning
+//
+//  Created by wchaos on 26.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-learningTests/IOS_learningTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_learningTests.swift
+//  IOS-learningTests
+//
+//  Created by wchaos on 26.08.2025.
+//
+
+import Testing
+@testable import IOS_learning
+
+struct IOS_learningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
